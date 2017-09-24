7831d2879adb3bc7f3c04d772e9ee16e1bcfc857
Asya Ivanova <asyaiv@yahoo.com>
2017-09-25 03:00:51 +1300

    Changes

---
+++ src/main/java/customerb/CustomerDetails.java
@@ -0,0 +1,63 @@
+package customerb;
+
+public class CustomerDetails {
+	
+	private String lname;
+	private String streetnumber;
+    private String fname;
+	private String timestamp;
+	private String cid;
+	private String zipcode;
+	private String city;
+	private String streetname;
+	
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getStreetnumber() {
+		return streetnumber;
+	}
+	public void setStreetnumber(String streetnumber) {
+		this.streetnumber = streetnumber;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getCid() {
+		return cid;
+	}
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getStreetname() {
+		return streetname;
+	}
+	public void setStreetname(String streetname) {
+		this.streetname = streetname;
+	}
+
+}
